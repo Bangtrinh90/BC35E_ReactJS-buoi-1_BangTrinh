4a9ed796966e757984465c76dc91a80910c17ce2
Bangtrinh90 <bangtrinhftu@gmail.com>
2022-10-31 19:32:38 +0700

    up 2

---
+++ reactbc35etrinh/src/components/BaiTapLayoutComponent/Item.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div>
+        <section className="pt-4">
+          <div className="container px-lg-5">
+            {/* Page Features*/}
+            <div className="row gx-lg-5">
+              <div className="col-lg-3 col-xxl-2 mb-5">
+                <div className="card bg-light border-0 h-325 w-500">
+                  <div className="card-body text-center p-2">
+                    <div className="feature p-5">
+                      <p className="text-center">500 x 325</p>
+                    </div>
+                    <div className="card-text">
+                      <h3 className="fs-4 fw-bold mb-2">Card title</h3>
+                      <p className="mb-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Praesentium totam, aperiam molestias laudantium
+                        dolor impedit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" card-btn border-top border-grey text-center">
+                    <button className="btn btn-primary text-white my-3">
+                      Find out more
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-xxl-2 mb-5">
+                <div className="card bg-light border-0 h-325 w-500">
+                  <div className="card-body text-center p-2">
+                    <div className="feature p-5">
+                      <p className="text-center">500 x 325</p>
+                    </div>
+                    <div className="card-text">
+                      <h3 className="fs-4 fw-bold mb-2">Card title</h3>
+                      <p className="mb-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Praesentium totam, aperiam molestias laudantium
+                        dolor impedit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" card-btn border-top border-grey text-center">
+                    <button className="btn btn-primary text-white my-3">
+                      Find out more
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-xxl-2 mb-5">
+                <div className="card bg-light border-0 h-325 w-500">
+                  <div className="card-body text-center p-2">
+                    <div className="feature p-5">
+                      <p className="text-center">500 x 325</p>
+                    </div>
+                    <div className="card-text">
+                      <h3 className="fs-4 fw-bold mb-2">Card title</h3>
+                      <p className="mb-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Praesentium totam, aperiam molestias laudantium
+                        dolor impedit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" card-btn border-top border-grey text-center">
+                    <button className="btn btn-primary text-white my-3">
+                      Find out more
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-xxl-2 mb-5">
+                <div className="card bg-light border-0 h-325 w-500">
+                  <div className="card-body text-center p-2">
+                    <div className="feature p-5">
+                      <p className="text-center">500 x 325</p>
+                    </div>
+                    <div className="card-text">
+                      <h3 className="fs-4 fw-bold mb-2">Card title</h3>
+                      <p className="mb-3">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Praesentium totam, aperiam molestias laudantium
+                        dolor impedit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" card-btn border-top border-grey text-center">
+                    <button className="btn btn-primary text-white my-3">
+                      Find out more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
--- reactbc35etrinh/src/index.js
+++ reactbc35etrinh/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/BaiTapLayoutComponent/Header.jsx";
+import Body from "./components/BaiTapLayoutComponent/Body.jsx";
+import Banner from "./components/BaiTapLayoutComponent/Banner.jsx";
+import Item from "./components/BaiTapLayoutComponent/Item.jsx";
+import Footer from "./components/BaiTapLayoutComponent/Footer.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <Body />
+    <Banner />
+    <Item />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
