0e6be487cbfea32558495b4f1d67a8f1ddf538b5
Bangtrinh90 <bangtrinhftu@gmail.com>
2022-11-03 00:47:36 +0700

    up2

--- reactbc35etrinh/src/index.js
+++ reactbc35etrinh/src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/BaiTapLayoutComponent/Header.jsx";
-import Body from "./components/BaiTapLayoutComponent/Body.jsx";
-import Banner from "./components/BaiTapLayoutComponent/Banner.jsx";
-import Item from "./components/BaiTapLayoutComponent/Item.jsx";
-import Footer from "./components/BaiTapLayoutComponent/Footer.jsx";
+import BaiTapThucHanhLayout from "./components/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx";
+
+// Nhúng css
+import "./assets/css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <Header />
-    <Body />
-    <Banner />
-    <Item />
-    <Footer />
+    <BaiTapThucHanhLayout />
   </div>
 );
